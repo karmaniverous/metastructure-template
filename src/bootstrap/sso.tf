7453e436e2ea9cd5095c3b65c0852d8b0278fb4e
Jason Williscroft <me@karmanivero.us>
2024-07-17 17:57:07 +0800

    added code headers

--- src/bootstrap/sso.tf
+++ src/bootstrap/sso.tf
@@ -1,3 +1,10 @@
+/*
+******************* DO NOT EDIT THIS NOTICE *****************
+This legal notice is added to every supported source code
+file at every commit. See the README for more info!                          
+*************************************************************
+*/
+
 ###############################################################################
 # Get the organization's SSO instance.
 ###############################################################################
