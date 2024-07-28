5c5051935d0c3b1c560b0c6ef54fa5d3f33d4761
Jason Williscroft <me@karmanivero.us>
2024-07-28 12:37:50 +0800

    added audit log bucket policy

--- src/bootstrap/audit_logs.tf
+++ src/bootstrap/audit_logs.tf
@@ -5,12 +5,118 @@ file at every commit. See the README for more info!
 *************************************************************
 */
 
+locals {
+  audit_log_bucket = "${module.global.config.organization.tokens.namespace}-${replace(module.global.config.organization.key_accounts.log_archive, "_", "-")}-${module.global.config.organization.tokens.audit_log}"
+}
+
+###############################################################################
+# Policy for AWS Config log delivery based on AWS Config Developer Guide.
+# https://docs.aws.amazon.com/config/latest/developerguide/s3-bucket-policy.html
+###############################################################################
+data "aws_iam_policy_document" "audit_log" {
+  provider = aws.key_account_log_archive
+  statement {
+    sid     = "AWSConfigBucketPermissionsCheck"
+    actions = ["s3:GetBucketAcl"]
+    principals {
+      type        = "Service"
+      identifiers = ["config.amazonaws.com"]
+    }
+    resources = ["arn:aws:s3:::${local.audit_log_bucket}"]
+  }
+
+  statement {
+    sid     = "AWSConfigBucketExistenceCheck"
+    actions = ["s3:ListBucket"]
+    principals {
+      type        = "Service"
+      identifiers = ["config.amazonaws.com"]
+    }
+    resources = ["arn:aws:s3:::${local.audit_log_bucket}"]
+  }
+
+  statement {
+    sid     = "AWSConfigBucketDelivery"
+    actions = ["s3:PutObject"]
+    principals {
+      type        = "Service"
+      identifiers = ["config.amazonaws.com"]
+    }
+    resources = concat(
+      ["arn:aws:s3:::${local.audit_log_bucket}/AWSLogs/${data.aws_caller_identity.me.account_id}/*"],
+      [
+        for account in module.global.config.accounts
+        : "arn:aws:s3:::${local.audit_log_bucket}/AWSLogs/${account.id}/Config/*"
+        if can(account.id) && !can(account.action)
+      ]
+    )
+
+    condition {
+      test     = "StringEquals"
+      variable = "s3:x-amz-acl"
+      values   = ["bucket-owner-full-control"]
+    }
+  }
+
+  statement {
+    sid = "AWSConfigBucketPermissionsCheckForMemberAccounts"
+    principals {
+      type = "AWS"
+      identifiers = [
+        for account in module.global.config.accounts
+        : "arn:aws:iam::${account.id}:root"
+        if can(account.id) && !can(account.action)
+      ]
+    }
+    actions   = ["s3:GetBucketAcl"]
+    resources = ["arn:aws:s3:::${local.audit_log_bucket}"]
+  }
+
+  statement {
+    sid = "AWSConfigBucketExistenceCheckForMemberAccounts"
+    principals {
+      type = "AWS"
+      identifiers = [
+        for account in module.global.config.accounts
+        : "arn:aws:iam::${account.id}:root"
+        if can(account.id) && !can(account.action)
+      ]
+    }
+    actions   = ["s3:ListBucket", "s3:GetBucketLocation"]
+    resources = ["arn:aws:s3:::${local.audit_log_bucket}"]
+  }
+
+  dynamic "statement" {
+    for_each = {
+      for k, v in module.global.config.accounts
+      : k => v
+      if can(v.id) && !can(v.action)
+    }
+
+    content {
+      sid = "AWSConfigBucketDeliveryFor${replace(title(replace(statement.key, "/[^a-z0-9]/", " ")), " ", "")}Account"
+      principals {
+        type        = "AWS"
+        identifiers = ["arn:aws:iam::${statement.value.id}:root"]
+      }
+      actions   = ["s3:PutObject"]
+      resources = ["arn:aws:s3:::${local.audit_log_bucket}/AWSLogs/${statement.value.id}/*"]
+
+      condition {
+        test     = "StringEquals"
+        variable = "s3:x-amz-acl"
+        values   = ["bucket-owner-full-control"]
+      }
+    }
+  }
+}
+
 ###############################################################################
 # Create audit log bucket at the key account assigned to log archive.
 ###############################################################################
 resource "aws_s3_bucket" "audit_log" {
   provider = aws.key_account_log_archive
-  bucket   = "${module.global.config.organization.tokens.namespace}-${replace(module.global.config.organization.key_accounts.log_archive, "_", "-")}-${module.global.config.organization.tokens.audit_log}"
+  bucket   = local.audit_log_bucket
 }
 
 ###############################################################################
@@ -36,3 +142,12 @@ resource "aws_s3_bucket_logging" "audit_log" {
   target_prefix = aws_s3_bucket.audit_log.bucket
 }
 
+###############################################################################
+# Attach audit log policy to the audit log bucket at the key account assigned 
+# to log archive.
+###############################################################################
+resource "aws_s3_bucket_policy" "audit_log" {
+  provider = aws.key_account_log_archive
+  bucket   = aws_s3_bucket.audit_log.id
+  policy   = data.aws_iam_policy_document.audit_log.json
+}
